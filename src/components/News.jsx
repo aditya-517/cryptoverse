5396ed31a722660222af26d46828f2cfabb09182
Aditya Jadhav <jadhavaditya517@gmail.com>
2022-05-23 09:34:38 +0530

    Updates

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -38,7 +38,7 @@ const News = ({simplified}) => {
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className='news-card'>
-            <a href = {news.url} target="_blank" rel="norefferer">
+            <a href = {news.url} target="_blank" rel="noreferrer">
               <div className='news-image-container'>
                 <Title className='news-title' level={4}>{news.name}</Title>
                 <img style={{maxWidth: '200px', maxHeight:'100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt='news'/>
